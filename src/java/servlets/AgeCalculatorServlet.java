41904d9f8702d82699ff7350b7dbab5d63f42207
DiegoWR <806509@SAIT225715IT.ACDM.DS.SAIT.CA>
2021-01-22 15:53:36 -0700

    Lab 3 Part 1 Done All working according to lab instructions.

--- src/java/servlets/AgeCalculatorServlet.java
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -20,12 +20,37 @@ public class AgeCalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String age = request.getParameter("enterAge");
+
+        request.setAttribute("enterAge", age);
         
+        if (age == null || age.equals("")) {
+            request.setAttribute("message", "You must give your current age");
+            getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+            return;
+        }
+        
+        for (int i = 0; i < age.length(); i++) {
+            char c = age.charAt(i);
+            if (c < '0' || c > '9') {
+                request.setAttribute("message", "You must enter a valid number");
+                getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                    .forward(request, response);
+                return;    
+            }
+        }
+        
+        request.setAttribute("message", "Your age next birthday will be " + (Integer.parseInt(age) + 1));
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
     }
 }
--- web/WEB-INF/agecalculator.jsp
+++ web/WEB-INF/agecalculator.jsp
@@ -9,9 +9,15 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Age Calculator</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Age Calculator</h1>
+        <form method="post" action="age">
+            Enter your age: <input type="text" name="enterAge" value="${age}"><br>
+            <input type="submit" value="Age next birthday"><br>
+            ${message}<br>
+            <a href="arithmetic">Arithmetic Calculator</a>
+        </form>
     </body>
 </html>
